a4cc29288685cb8f30cd674a5bd0ef8a341c06df
tangwang <10799359+kingt527@user.noreply.gitee.com>
2025-09-08 18:37:01 +0800

    prod.env.js 配置更新

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  BASE_API: '"https://admin-api.macrozheng.com"'
+  // BASE_API: '"https://admin-api.macrozheng.com"'
+  BASE_API: '"http://3.19.61.10:8201/mall-admin"'
 }
